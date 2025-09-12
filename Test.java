8b47a332a4b20d61070ab60bfbaf2799f85e8215
mdadilfaiz48 <mdadilfaiz48@gmail.com>
2025-09-12 03:09:57 -0700

    garbageCollection added

---
+++ Test.java
@@ -0,0 +1,16 @@
+import java.util.Date;
+
+public class Test {
+    public static void main(String[] args){
+        Runtime r = Runtime.getRuntime();
+        System.out.println(r.totalMemory());
+        System.out.println(r.freeMemory());
+        for(int i = 0;i<10000;i++){
+            Date d = new Date();
+            d = null;
+        }
+        System.out.println(r.freeMemory());
+        r.gc();
+        System.out.println(r.freeMemory());
+    }
+}
